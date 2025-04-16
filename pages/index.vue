abb53b395132d5625a86a02938858c68c0d1e901
guen <heloo@guen.dev>
2025-04-16 17:17:33 +0700

    add confetti effect on click and include party-js dependency

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import { confetti, variation } from 'party-js'
 
+function play(source: HTMLDivElement | MouseEvent) {
+    confetti(source, {
+        count: variation.range(20, 40)
+    })
+}
 </script>
 
 <template>
-  <div>
+  <div @click="play">
     <invitation-overview />
   </div>
 </template>
